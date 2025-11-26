25c5f9341ca7dc8f7482c9e841fd7fe85fc4e989
Onur Akman <ahmetonurakman@gmail.com>
2025-11-26 17:03:35 +0100

    Change base path for deployment in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/urb_web/', // Sets the base path for deployment to https://<USERNAME>.github.io/urb_web/
+  base: '/',  // correct for custom domains
 })
